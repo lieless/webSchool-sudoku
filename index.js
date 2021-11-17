1e12602c45e98d2898261bfb99c89a3ea2ca3594
mirtesenn <murdalova.leyla@yandex.ru>
2021-11-17 16:55:40 +0300

    add solveFunc

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-let solveFunc = function(desk) {
+let solveFunc = function (desk) {
+    const solver = () => {
+        const curPos = findZero(desk)
+        if (curPos == false) {
+            return true
+        }
+        for (let i = 1; i < 10; i++) {
+            const curNum = i
+            const isValid = toValidate(curNum, curPos, desk)
+            if (isValid) {
+                const [x, y] = curPos
+                desk[x][y] = curNum
+                if (solver()) {
+                    return true
+                }
+                desk[x][y] = 0
+            }
+        }
+        return false
+    }
+    solver()
     return desk
 }
