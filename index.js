db8661a9ea57c2d8d3a46cd0adeca3f0ebfd54a9
mirtesenn <murdalova.leyla@yandex.ru>
2021-11-17 17:13:16 +0300

    final commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let solveFunc = function(desk) {
-    const findZero  = (desk) => {
-        for(let row=0; row<9; row++) {
-            for(let col=0; col<9; col++) {
-                if(desk[row][col]==0) {
+let solveFunc = function (desk) {
+    const findZero = (desk) => {
+        for (let row = 0; row < 9; row++) {
+            for (let col = 0; col < 9; col++) {
+                if (desk[row][col] == 0) {
                     return [row, col]
                 }
             }
@@ -11,24 +11,44 @@ let solveFunc = function(desk) {
     }
     const toValidate = (num, pos, desk) => {
         const [row, col] = pos
-        for(let i = 0; i<9; i++) {
-            if(desk[i][col]===num && i !== row) { 
+        for (let i = 0; i < 9; i++) {
+            if (desk[i][col] === num && i !== row) {
                 return false
             }
         }
-        for(let i = 0; i<9; i++) {
-            if(desk[row][i]===num && i !== col) {
+        for (let i = 0; i < 9; i++) {
+            if (desk[row][i] === num && i !== col) {
                 return false
             }
         }
-        for(let i=(Math.floor(row/3)*3); i<((Math.floor(row/3)*3)+3); i++) {
-            for(let j=(Math.floor(col/3)*3); j<((Math.floor(col/3)*3)+3); j++) {
-                if(desk[i][j]===num && i!==row && j!==col) {
+        for (let i = (Math.floor(row / 3) * 3); i < ((Math.floor(row / 3) * 3) + 3); i++) {
+            for (let j = (Math.floor(col / 3) * 3); j < ((Math.floor(col / 3) * 3) + 3); j++) {
+                if (desk[i][j] === num && i !== row && j !== col) {
                     return false
                 }
             }
         }
         return true
     }
+    const solver = () => {
+        const curPos = findZero(desk)
+        if (curPos == false) {
+            return true
+        }
+        for (let i = 1; i < 10; i++) {
+            const curNum = i
+            const isValid = toValidate(curNum, curPos, desk)
+            if (isValid) {
+                const [x, y] = curPos
+                desk[x][y] = curNum
+                if (solver()) {
+                    return true
+                }
+                desk[x][y] = 0
+            }
+        }
+        return false
+    }
+    solver()
     return desk
 }
